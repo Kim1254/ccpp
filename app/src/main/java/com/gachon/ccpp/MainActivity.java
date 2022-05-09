0f35225521299e4f28086915c06be7d058627dd2
KyeongMin <93812148+KyeongMin5307@users.noreply.github.com>
2022-05-09 15:27:56 +0900

    added jsonparsing on mainactivity

--- app/src/main/java/com/gachon/ccpp/MainActivity.java
+++ app/src/main/java/com/gachon/ccpp/MainActivity.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.gachon.ccpp.util.DataHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView2);
         Intent intent = getIntent();
+
         String text = intent.getStringExtra("html");
-        textView.setText(text);
+
+        String out = "";
+
+        JSONObject data = null;
+        try {
+            data = DataHandler.parse(text);
+            for (Iterator<String> iter = data.keys(); iter.hasNext();) {
+                out += (String)data.get(iter.next()) + "\n";
+            }
+        } catch (JSONException e) {
+            Log.d("CCPP", "Failed parsing json on onCreate(html)");
+        }
+        textView.setText(out);
     }
 }
