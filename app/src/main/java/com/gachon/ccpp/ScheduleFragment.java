287d387737606a86dfe217afe23f9dd63e819955
KyeongMin Kim <k001202@gachon.ac.kr>
2022-05-28 15:01:58 +0900

    Simple calendar

--- app/src/main/java/com/gachon/ccpp/ScheduleFragment.java
+++ app/src/main/java/com/gachon/ccpp/ScheduleFragment.java
@@ -1,22 +1,66 @@
 package com.gachon.ccpp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
 
 public class ScheduleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View thisView = inflater.inflate(R.layout.fragment_lecture, container, false);
+        View thisView = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        MaterialCalendarView cal = thisView.findViewById(R.id.calendar);
+        cal.setSelectionColor(
+                ResourcesCompat.getColor(getResources(), R.color.gcc_green, null));
+        cal.setSelectedDate(CalendarDay.today());
+
+        cal.addDecorators(
+                new DateDecorator(Calendar.SATURDAY,
+                        ResourcesCompat.getColor(getResources(), R.color.gcc_liteblue, null)),
+                new DateDecorator(Calendar.SUNDAY,
+                        ResourcesCompat.getColor(getResources(), R.color.gcc_orange, null))
+        );
 
         return thisView;
+    }
+
+    static class DateDecorator implements DayViewDecorator {
+        private final Calendar cal = Calendar.getInstance();
+
+        private final int date;
+        private final int color;
+
+        DateDecorator(int date, int color) {
+            this.date = date;
+            this.color = color;
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(cal);
+            return cal.get(Calendar.DAY_OF_WEEK) == date;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(color));
+        }
     }
 }
