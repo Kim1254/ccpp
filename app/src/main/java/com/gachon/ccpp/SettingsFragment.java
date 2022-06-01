ae0183e70a5d713723cb3cef3675c72bf551540d
001021 <84309420+001021@users.noreply.github.com>
2022-06-01 19:47:05 +0900

    Setting 화면 구현

--- app/src/main/java/com/gachon/ccpp/SettingsFragment.java
+++ app/src/main/java/com/gachon/ccpp/SettingsFragment.java
@@ -2,28 +2,70 @@ package com.gachon.ccpp;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
-import androidx.preference.ListPreference;
-import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
+//    SharedPreferences prefs;
+//
+//    Preference keywordLanguagePreference;
+//
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        addPreferencesFromResource(R.xml.settings_preference);
+//        keywordLanguagePreference = findPreference("language_list");
+//
+//        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+//
+//    }
+
+    private static final String SETTING_NOTIFICATION = "notification_alarm";
+    private static final String SETTING_ASSIGNMENT = "assignment_alarm";
+    private static final String SETTING_COMMENT = "comment_alarm";
+    private static final String SETTING_LANGUAGE = "language_list";
     SharedPreferences prefs;
 
-    ListPreference alarmPreference;
-    ListPreference keywordLanguagePreference;
-    PreferenceScreen keywordScreen;
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_preference);
-        alarmPreference = (ListPreference) findPreference("language_list");
-        keywordAlarmPreference = (ListPreference) findPreference("keyword_")
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        prefs.registerOnSharedPreferenceChangeListener(prefListener);
     }
+
+    SharedPreferences.OnSharedPreferenceChangeListener prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(SETTING_NOTIFICATION)) {
+                Log.d("TAG", key + "SELECTED");
+            }
+            if (key.equals(SETTING_ASSIGNMENT)) {
+                Log.d("TAG", key + "SELECTED");
+            }
+            if (key.equals(SETTING_COMMENT)) {
+                Log.d("TAG", key + "SELECTED");
+            }
+            if (key.equals(SETTING_LANGUAGE)) {
+                Log.d("TAG", key + "SELECTED");
+            }
+        }
+    };
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_setting, container, false);
+//    }
 
 
 }
